d4c03093c00902f41928d0cebbd417a4d3bff4e5
MrFrooty <fredsong99@gmail.com>
2023-05-23 13:22:32 -0700

    Create initKeybinds() and few other functions.

--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -1,1 +1,23 @@
 #include "MainMenuState.h"
+
+void MainMenuState::initKeybinds() {
+    std::ifstream ifs("Config/gamestate_keybinds.ini");
+
+    if(ifs.is_open()) {
+        std::string key = "";
+        std::string key2 = "";
+
+        while(ifs >> key >> key2) {
+            this->keybinds[key] = this->supportedKeys->at(key2);
+        }
+    }
+    ifs.close();
+}
+
+MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys) : State(window, supportedKeys) {
+    this->initKeybinds();
+}
+
+MainMenuState::~MainMenuState() {
+    
+}
