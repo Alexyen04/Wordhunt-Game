3decc6aa45fc075031978d29c3f7482885801810
mchen04 <michaelluochen1@gmail.com>
2023-05-23 14:06:43 -0700

    Game.cpp Creation

---
+++ Game.cpp
@@ -0,0 +1,49 @@
+#include "Game.h"
+
+
+void Game::initWindow()
+{
+   this -> window = new sf::RenderWindow(sf::VideoMode(800, 600), "C++ SFML WordHunt") ;
+
+
+}
+
+
+Game::Game()
+{
+
+
+}
+
+
+Game::~Game()
+{
+   delete this -> window ;
+}
+
+
+void Game::update()
+{
+
+
+}
+
+
+void Game::render()
+{
+
+
+}
+
+
+void Game::run()
+{
+  
+}
+
+
+void Game::updateSFMLevents()
+{
+
+
+}
