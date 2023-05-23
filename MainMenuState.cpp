79e63dc9aad8f9c69cae2c776a2935ff702334c3
MrFrooty <fredsong99@gmail.com>
2023-05-23 13:27:01 -0700

    Finish initial MainMenuState commits.

--- MainMenuState.cpp
+++ MainMenuState.cpp
@@ -19,5 +19,39 @@ MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int
 }
 
 MainMenuState::~MainMenuState() {
-    
+
+}
+
+void MainMenuState::endState() {
+    std::cout << "Ending GameState" << "\n";
+}
+
+void MainMenuState::updateInput(const float & dt) {
+    this->checkForQuit();
+
+    //update player input
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.get("MOVE_LEFT")))) {
+        this->player.move(dt, -1.f, 0.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.get("MOVE_RIGHT")))) {
+        this->player.move(dt, 1.f, 0.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.get("MOVE_UP")))) {
+        this->player.move(dt, 0.f, -1.f);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key(this->keybinds.get("MOVE_DOWN")))) {
+        this->player.move(dt, 0.f, 1.f);
+    }
+}
+
+void MainMenuState::update(const float& dt) {
+    this->updateInput(dt);
+    this->player.update(dt);
+}
+
+void MainMenuState::render(sf::RenderTarget* target) {
+    if(!target) {
+        target = this->window;
+    }
+    this->player.render(target);
 }
