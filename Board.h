97883205d0bcac872325c6ccad7194ef170e37ae
mchen04 <michaelluochen1@gmail.com>
2023-05-30 16:50:33 -0700

    Created Board that shows

--- Board.h
+++ Board.h
@@ -1,36 +1,81 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <sstream>
-#include <vector>
+#include <SFML/Graphics.hpp>
 #include "piece.h"
 
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Window.hpp>
+class Board
+{
+public:
+    Board(unsigned int dimensions)
+        : dimensions(dimensions), hoverPiece(nullptr)
+    {
+        // Set the window size to 1500x1500
+        int windowSize = 1500;
 
-class Board {
-    public:
-        Board (float x, float y, float width, float height, 
-        sf::Font* font, std::string text) ;
+        // Calculate the total size of the board
+        board.setSize(sf::Vector2f(windowSize, windowSize));
+        board.setFillColor(sf::Color(200, 200, 200)); // Light gray color
 
-        ~Board();
+        // Create the grid of pieces
+        pieces.reserve(dimensions * dimensions);
+        float pieceSize = static_cast<float>(windowSize) / dimensions;
+        for (unsigned int row = 0; row < dimensions; ++row)
+        {
+            for (unsigned int col = 0; col < dimensions; ++col)
+            {
+                float x = col * pieceSize;
+                float y = row * pieceSize;
+                pieces.emplace_back(x, y, pieceSize, 'A' + (row * dimensions + col));
+            }
+        }
+    }
 
-        void fillBoard(int, int);
-        
-        void render(sf::RenderTarget* target);
+    void handleEvent(const sf::Event& event)
+    {
+        if (event.type == sf::Event::MouseMoved)
+        {
+            // Get the mouse position relative to the board
+            sf::Vector2f mousePosition = sf::Vector2f(event.mouseMove.x, event.mouseMove.y) - board.getPosition();
 
+            // Find the piece under the mouse position
+            hoverPiece = nullptr;
+            for (auto& piece : pieces)
+            {
+                if (piece.getGlobalBounds().contains(mousePosition))
+                {
+                    hoverPiece = &piece;
+                    break;
+                }
+            }
+        }
+    }
 
-    private:
-        int score;
-        std::vector <Piece> boardChars;
-        sf::RectangleShape shape ;
-        sf::Font* font ;
-        sf::Text text ;
+    void render(sf::RenderTarget& target) const
+    {
+        target.draw(board);
+        for (const auto& piece : pieces)
+        {
+            piece.render(target);
 
+            // If a piece is being hovered over, change its color to green
+            if (&piece == hoverPiece)
+            {
+                sf::FloatRect bounds = piece.getGlobalBounds();
+                sf::RectangleShape highlight;
+                highlight.setSize(sf::Vector2f(bounds.width, bounds.height));
+                highlight.setPosition(bounds.left, bounds.top);
+                highlight.setFillColor(sf::Color::Green);
+                target.draw(highlight);
+            }
+        }
+    }
+
+private:
+    unsigned int dimensions;
+    sf::RectangleShape board;
+    std::vector<Piece> pieces;
+    const Piece* hoverPiece;
 };
 
 #endif
