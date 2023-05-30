cf01fbee5c7cd422fea08618605a974c9a4677bb
mchen04 <michaelluochen1@gmail.com>
2023-05-30 16:50:53 -0700

    piece that make sup board

--- piece.h
+++ piece.h
@@ -1,29 +1,81 @@
 #ifndef PIECE_H
 #define PIECE_H
 
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-#include <sstream>
-
 #include <SFML/Graphics.hpp>
 
-class Piece {
-    public: 
-        Piece(float x, float y, float width, float height, 
-        sf::Font* font, std::string text, 
-        sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor) ;
+class Piece
+{
+public:
+    Piece(float x, float y, float size, char letter)
+        : position(sf::Vector2f(x, y)), size(size), letter(letter), isHovered(false)
+    {
+        shape.setPosition(position);
+        shape.setSize(sf::Vector2f(size, size));
+        shape.setOutlineThickness(2.0f);
+        shape.setOutlineColor(sf::Color::Black);
 
-        ~Piece() ;        
-    private:
-        sf::RectangleShape shape ;
-        sf::Font* font ;
-        sf::Text text ;
-        char c; 
+        text.setString(std::string(1, letter));
+        text.setCharacterSize(static_cast<unsigned int>(size * 0.4f)); // Adjust the character size based on the piece size
+        text.setFillColor(sf::Color::Black);
+        text.setStyle(sf::Text::Bold);
 
-        sf::Color idleColor ;
-        sf::Color hoverColor ;
-        sf::Color activateColor ;
+        sf::FloatRect bounds = text.getLocalBounds();
+        text.setOrigin(bounds.left + bounds.width / 2.0f, bounds.top + bounds.height / 2.0f);
+        text.setPosition(position.x + size / 2.0f, position.y + size / 2.0f);
+    }
+
+    void render(sf::RenderTarget& target) const
+    {
+        target.draw(shape);
+        target.draw(text);
+
+        // If the piece is being hovered over, change its fill color to green
+        if (isHovered)
+        {
+            sf::RectangleShape highlight(shape.getSize());
+            highlight.setPosition(position);
+            highlight.setFillColor(sf::Color::Green);
+            target.draw(highlight);
+        }
+    }
+
+    sf::FloatRect getGlobalBounds() const
+    {
+        return shape.getGlobalBounds();
+    }
+
+    bool getIsHovered() const
+    {
+        return isHovered;
+    }
+
+    void setIsHovered(bool hovered)
+    {
+        isHovered = hovered;
+    }
+
+    char getLetter() const
+    {
+        return letter;
+    }
+
+    void setLetter(char newLetter)
+    {
+        letter = newLetter;
+        text.setString(std::string(1, letter));
+
+        sf::FloatRect bounds = text.getLocalBounds();
+        text.setOrigin(bounds.left + bounds.width / 2.0f, bounds.top + bounds.height / 2.0f);
+        text.setPosition(position.x + size / 2.0f, position.y + size / 2.0f);
+    }
+
+private:
+    sf::Vector2f position;
+    float size;
+    char letter;
+    bool isHovered;
+    sf::RectangleShape shape;
+    sf::Text text;
 };
 
-#endif  
+#endif
